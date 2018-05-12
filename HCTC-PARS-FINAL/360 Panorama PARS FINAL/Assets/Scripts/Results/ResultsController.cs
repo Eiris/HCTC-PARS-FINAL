30d7fa92083c75c445384b3949577b467077e17e
iTwicKz <twickes32@ufl.edu>
2018-05-12 10:22:02 -0400

    Added overall score

--- HCTC-PARS-FINAL/360 Panorama PARS FINAL/Assets/Scripts/Results/ResultsController.cs
+++ HCTC-PARS-FINAL/360 Panorama PARS FINAL/Assets/Scripts/Results/ResultsController.cs
@@ -7,16 +7,25 @@ public class ResultsController : MonoBehaviour {
 
 	public ResultCardCell ResultCardCell;
 	public AssessmentController controller;
+	public Text overallScoreLabel;
 
 	// Use this for initialization
 	void Start () {
 		controller = FindObjectOfType(typeof(AssessmentController)) as AssessmentController;
+		float overallScore = 0.0f;
 		foreach (AssessmentRound result in controller.finishedRounds) {
 			var newCell = Instantiate(ResultCardCell, transform);
 			newCell.transform.parent = transform;
 			newCell.populateAssessmentResults(result, controller.answerKey);
+			overallScore += result.HIIScore;
 		}
+		this.populateOverallScore(overallScore / (float)controller.finishedRounds.Count);
 	}
+
+	void populateOverallScore(float overallScore) {
+		overallScoreLabel.text = "YOUR OVERALL SCORE: " + overallScore.ToString() + "%";
+	}
+	
 	
 	// Update is called once per frame
 	void Update () {
